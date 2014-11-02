926c2b5768d3759cc4e75cbf24cf7825a70aeeb7
altaweelali <altaweel.ali@me.com>
2014-11-02 09:32:06 -0600

    pie chart

--- js/app/controllers/summary.controller.js
+++ js/app/controllers/summary.controller.js
@@ -2,5 +2,18 @@
 var app = angular.module('app')
 app.controller('summaryCtrl', function ($scope, $ionicModal, $timeout, $http, auth, $state, datacontext, $rootScope) {
 
+    var width = $(document).width() * 0.70;
+    $scope.pieOptions = {
+        animate: {
+            duration: 1000,
+            enabled: true
+        },
+        size: width,
+        barColor: '#2b6b90',
+        scaleColor: false,
+        lineWidth: 14,
+        // lineCap: 'circle'
+    };
 
+    $scope.percentComplete = 75;
 })
